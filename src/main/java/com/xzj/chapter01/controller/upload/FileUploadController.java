bff7e28db5dd8e7666137a144d0407b5fd28a73c
chenkunwei1995 <m17854225838@163.com>
2020-01-05 22:24:56 +0800

    文件上传遗漏提交

--- src/main/java/com/xzj/chapter01/controller/upload/FileUploadController.java
+++ src/main/java/com/xzj/chapter01/controller/upload/FileUploadController.java
@@ -1,5 +1,8 @@
 package com.xzj.chapter01.controller.upload;
 
+import com.xzj.chap1.model.basemodel.FileBase;
+import com.xzj.helper.file.txt.TxtFileOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -13,13 +16,22 @@ import java.util.logging.Logger;
 
 @RestController
 public class FileUploadController {
-    SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd");
+
+
+    @Autowired
+    FileBase fileBase;
+
+    SimpleDateFormat sdf=new SimpleDateFormat("yyyy/MM/dd/");
+    SimpleDateFormat txtpathsdf=new SimpleDateFormat("yyyy/MM/");
+    SimpleDateFormat dateFormat=new SimpleDateFormat("yyyy-MM-dd");
     @RequestMapping("/upload")
     public String upload(MultipartFile uploadFile, HttpServletRequest req){
-        String relpath=req.getSession().getServletContext().getRealPath("/uploadFile/");
+        String relpath=fileBase.getImgfile();//req.getSession().getServletContext().getRealPath("/uploadFile/");
         String format=sdf.format(new Date());
         File folder=new File(relpath+format);
         if(!folder.isDirectory()){
+            if(!folder.getParentFile().exists())
+                folder.getParentFile().mkdirs();
             folder.mkdir();
         }
         String oldName=uploadFile.getOriginalFilename();
@@ -27,7 +39,17 @@ public class FileUploadController {
         String newName= UUID.randomUUID().toString()+oldName.substring(oldName.lastIndexOf("."),oldName.length());
         try{
             uploadFile.transferTo(new File(folder,newName));
-            String filePath=req.getScheme()+"://"+req.getServerName()+":"+req.getServerPort()+"/uploadFile/"+format+newName;
+            String filePath=req.getScheme()+"://"+req.getServerName()+":"+req.getServerPort()+"/uploadFile/imgfile/"+format+newName;
+            String txtPath= fileBase.getTxtfile()+txtpathsdf.format(new Date()); // req.getSession().getServletContext().getRealPath("/txt2019/");
+            File txtfolder=new File(txtPath);
+            if(!txtfolder.isDirectory()){
+               if(!txtfolder.getParentFile().exists()){
+                   txtfolder.getParentFile().mkdirs();
+               }
+                txtfolder.mkdir();
+            }
+            txtPath+=dateFormat.format(new Date())+".txt";
+            TxtFileOperation.writeFile(txtPath,filePath);
             return filePath;
         }catch(Exception ex){
             ex.printStackTrace();
